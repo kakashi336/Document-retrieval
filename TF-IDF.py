602e7fec69d8bf66eea5d001d4e6fa3357c6b218
Arjun P <arjunp111009@gmail.com>
2020-06-11 13:49:39 +0530

    Add files via upload

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,31 +1,40 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
+import math
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# In[7]:
+def computeTF(WordDict,BoW):
+    tfDict={}
+    for words,count in WordDict.items():
+        tfDict[words]=count/len(BoW)
+    return tfDict
 
+def computeIDF(docs):
+    idfDict=dict.fromkeys(docs[0],0)
+    N=len(docs)
+    for documents in docs:
+        for word,val in documents.items():
+            if val>0:
+                idfDict[word]+=1
+    for word,val in idfDict.items():
+        idfDict[word]=math.log(N/float(val))
+    print(idfDict)
+    return idfDict
 
-documentA = 'the man went out for a walk'
+def computeTFIDF(TF,IDF):
+    tfidf={}
+    for words,val in TF.items():
+        tfidf[words]=val*IDF[words]
+    print(tfidf)
+    return tfidf
+
+documentA ='the man went out for a walk'
 documentB="the man went out but he never came back coz he dead motherfucerrrr"
-
-
-# In[8]:
-
 
 BowA=documentA.split()
 BowB=documentB.split()
 uniqueWords=set(BowA).union(set(BowB))
-
-
-# In[9]:
-
 
 numOfWordsA=dict.fromkeys(uniqueWords,0)
 numOfWordsB=dict.fromkeys(uniqueWords,0)
@@ -36,66 +45,10 @@ for w in BowB:
     numOfWordsB[w]+=1
 
 
-# In[10]:
-
-
-def computeTF(WordDict,BoW):
-    tfDict={}
-    for words,count in WordDict.items():
-        tfDict[words]=count/len(BoW)
-    return tfDict
 tfA = computeTF(numOfWordsA,BowA)
 tfB = computeTF(numOfWordsB,BowB)
 
-
-# In[11]:
-
-
-def computeIDF(docs):
-    idfDict=dict.fromkeys(docs[0],0)
-    import math
-    N=len(docs)
-  #  print(docs,N)
-    for documents in docs:
-        for word,val in documents.items():
-            if val>0:
-                idfDict[word]+=1
-  #  print(idfDict)
-    for word,val in idfDict.items():
-        idfDict[word]=math.log(N/float(val))
-    print(idfDict)
-    return idfDict
-
 idfs = computeIDF([numOfWordsA, numOfWordsB])
 
-
-# In[ ]:
-
-
-
-
-
-# In[12]:
-
-
-def computeTFIDF(TF,IDF):
-    tfidf={}
-    for words,val in TF.items():
-        tfidf[words]=val*IDF[words]
-    print(tfidf)
-    return tfidf
 a=computeTFIDF(tfA,idfs)
 b=computeTFIDF(tfB,idfs)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
